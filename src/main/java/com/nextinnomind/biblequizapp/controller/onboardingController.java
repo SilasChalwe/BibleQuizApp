f89462cf8f5295e3dfcdea32c2d457af86b3092e
Silas Chalwe <SilasChalwe@outlook.com>
2025-08-03 11:29:45 +0200

    Add onboardingController for first-time user setup

--- src/main/java/com/nextinnomind/biblequizapp/controller/onboardingController.java
+++ src/main/java/com/nextinnomind/biblequizapp/controller/onboardingController.java
@@ -1,47 +1,28 @@
 package com.nextinnomind.biblequizapp.controller;
 
-import com.nextinnomind.biblequizapp.AppLoader.DesktopViewLoader;
-import com.nextinnomind.biblequizapp.AppLoader.MobileViewLoader;
-import com.nextinnomind.biblequizapp.AppManager.ViewModeSelectorManager;
+import com.nextinnomind.biblequizapp.Loader.ViewLoader;
+import com.nextinnomind.biblequizapp.Manager.ViewModeSelectorManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
 
 public class onboardingController {
 
     private static final Logger logger = LogManager.getLogger(onboardingController.class);
 
-    @FXML
-    private Button playButton;
 
     private final ViewModeSelectorManager viewModeSelectorManager = new ViewModeSelectorManager();
 
     @FXML
     private void handlePlayButton(ActionEvent event) {
-        try {
-            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            String viewMode = viewModeSelectorManager.getViewMode();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        String viewMode = viewModeSelectorManager.getViewMode();
+        logger.info("Play button clicked. Detected view mode: {}", viewMode);
+        ViewLoader.loadMainView(window,viewMode,"/com/nextinnomind/biblequizapp/views/levels-view.fxml");
 
-            logger.info("Play button clicked. Detected view mode: {}", viewMode);
-
-            String fxmlPath = "/com/nextinnomind/biblequizapp/views/levels-view.fxml";
-
-            if ("mobile".equalsIgnoreCase(viewMode)) {
-                MobileViewLoader.load(window, fxmlPath);
-                logger.info("Loaded mobile view: {}", fxmlPath);
-            } else {
-                DesktopViewLoader.load(window, fxmlPath);
-                logger.info("Loaded desktop view: {}", fxmlPath);
-            }
-
-        } catch (IOException e) {
-            logger.error("Error loading levels view FXML", e);
-        }
     }
 }
